38bad4d457b71c1c5e2a1137b9e041dd6429353a
Clifford Toledo <cliffordjohntoledo@gmail.com>
2021-09-22 19:24:26 -0400

    UI

--- src/main/java/interfaces/LoginInterface.java
+++ src/main/java/interfaces/LoginInterface.java
@@ -1,10 +1,41 @@
 package interfaces;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class LoginInterface extends JPanel {
+public class LoginInterface extends JFrame implements ActionListener {
 
-    public static void main(String[] args) {
+    private JPanel panel;
+    private JButton exitButton;
+    private JTextField pinField;
+    private JTextPane textPane;
+
+    public LoginInterface() {
+        panel = new JPanel();
+
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+        setUndecorated(true);
+        setResizable(false);
+        setVisible(true);
+
+        exitButton = new JButton("EXIT");
+        exitButton.setSize(40, 20);
+        panel.add(exitButton);
+        exitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        pinField = new JTextField();
+
+        textPane = new JTextPane();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
 
     }
 }
