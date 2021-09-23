0a8c7a42048671589f75edb9e3fc506551f509e3
Clifford Toledo <cliffordjohntoledo@gmail.com>
2021-09-23 09:23:39 -0400

    Update

--- src/main/java/POSSystemApplication.java
+++ src/main/java/POSSystemApplication.java
@@ -1,14 +1,17 @@
 import controllers.EmployeeManager;
 import controllers.ItemManager;
+import interfaces.LoginInterface;
 import models.Employee;
 import models.Item;
 
+import javax.swing.*;
+import java.awt.*;
 import java.util.HashMap;
 import java.util.Map;
 
 public class POSSystemApplication {
 
     public static void main(String[] args) {
-        System.out.print(((int) (Math.random() * 9999)) + "");
+        LoginInterface loginInterface = new LoginInterface();
     }
 }
--- src/main/java/interfaces/LoginInterface.java
+++ src/main/java/interfaces/LoginInterface.java
@@ -1,33 +1,40 @@
 package interfaces;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class LoginInterface extends JFrame implements ActionListener {
 
-    private JPanel panel;
+    private JPanel mainPanel;
+    private JPanel exitPanel;
     private JButton exitButton;
     private JTextField pinField;
     private JLabel label;
 
     public LoginInterface() {
-        panel = new JPanel();
+        pinField = new JTextField();
+
+        label = new JLabel();
+
+        exitButton = new JButton("EXIT");
+        exitButton.setPreferredSize(new Dimension(200, 100));
+        exitButton.addActionListener(this);
+
+        exitPanel = new JPanel(new BorderLayout());
+        exitPanel.add(exitButton, BorderLayout.LINE_END);
+
+        mainPanel = new JPanel();
+        mainPanel.setLayout(new BorderLayout());
+        mainPanel.add(exitPanel, BorderLayout.PAGE_END);
+
+        add(mainPanel);
 
         setExtendedState(JFrame.MAXIMIZED_BOTH);
         setUndecorated(true);
         setResizable(false);
         setVisible(true);
-
-        exitButton = new JButton("EXIT");
-        exitButton.setSize(40, 20);
-        panel.add(exitButton);
-        exitButton.addActionListener(this);
-        add(exitButton);
-
-        pinField = new JTextField();
-
-        label = new JLabel();
     }
 
     @Override
