d588510884b8cf3bd7750a544ee1fdeeccf96a7a
Clifford Toledo <cliffordjohntoledo@gmail.com>
2021-10-01 11:28:55 -0400

    Testing Interfaces

--- src/main/java/interfaces/MainUI.java
+++ src/main/java/interfaces/MainUI.java
@@ -1,14 +1,42 @@
 package interfaces;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class MainUI {
+public class MainUI extends JFrame implements ActionListener {
 
     private JButton orderButton;
     private JButton accountButton;
     private JButton logoutButton;
+    private JPanel optionsPanel;
 
     public MainUI() {
+        add(optionsPanel);
+        setSize(new Dimension(Toolkit.getDefaultToolkit().getScreenSize()));
+        setUndecorated(true);
+        setResizable(false);
+        setVisible(true);
 
+        orderButton.addActionListener(this);
+
+        accountButton.addActionListener(this);
+
+        logoutButton.addActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == orderButton) {
+
+        }
+        if (e.getSource() == accountButton) {
+
+        }
+        if (e.getSource() == logoutButton) {
+            new LoginUI();
+        }
+        dispose();
     }
 }
---
+++ src/main/java/interfaces/OrderUI.java
@@ -0,0 +1,20 @@
+package interfaces;
+
+import javax.swing.*;
+
+public class OrderUI {
+    private JButton SMALLButton;
+    private JButton MEDIUMButton;
+    private JButton LARGEButton;
+    private JButton button6;
+    private JButton button1;
+    private JButton BACONButton;
+    private JButton PEPPERONIButton;
+    private JButton SAUSAGEButton;
+    private JButton PEPPERSButton;
+    private JButton EXTRACHEESEButton;
+    private JButton ONIONButton1;
+    private JButton MUSHROOMButton1;
+    private JButton PINEAPPLEButton;
+    private JTable table1;
+}
